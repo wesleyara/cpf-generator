ba67a98dfc58380a64c420bf78f55a1161c390cb
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-14 12:36:31 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/index.css";
+import App from "./components/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
