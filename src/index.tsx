fce3b72b08a954a9f31d6d9a8d6fde822c5e5e42
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-15 16:01:00 -0300

    :wrench: Alterando rotas do css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./styles/index.css";
+import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(
