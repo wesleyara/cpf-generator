39cf0f48ea16913ba7928e35c92ef4ea23f874d1
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2022-01-25 16:39:18 -0300

    :wrench: Alternado rotas de styles

--- src/components/components-secondary/Gerador.tsx
+++ src/components/components-secondary/Gerador.tsx
@@ -1,14 +1,60 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from "react";
-import style from "../components-styles/Gerador.module.css";
-import Progresso from "./Progresso";
+import React, { useEffect, useState } from "react";
+import style from "../components-styles/Gerador.module.scss";
 
 export default function Gerador() {
   const [renderiza, setRenderiza] = useState(false);
 
+  useEffect(() => {
+    const progresso = document.querySelector("progress") as HTMLProgressElement;
+    const spann = document.querySelector("span") as HTMLSpanElement;
+
+    let porcentagem = 0;
+
+    setInterval(() => {
+      if (porcentagem < 100) {
+        porcentagem += 1;
+        progresso.value = porcentagem;
+        spann.textContent = `${porcentagem}%`;
+      }
+
+      if (porcentagem == 50) {
+        spann.style.cssText = `
+                color:white;
+                `;
+      }
+    }, 100);
+  });
+
   function gerar() {
     setRenderiza(true);
+    setRenders(false);
+
+    setTimeout(() => {
+      setRenderiza(false);
+      setRenders(true);
+    }, 10000);
   }
+
+  const [renders, setRenders] = useState(false);
+
+  const n01: any = Math.random() * 10;
+  const n02: any = Math.random() * 10;
+  const n03: any = Math.random() * 10;
+  const n04: any = Math.random() * 10;
+  const n05: any = Math.random() * 10;
+  const n06: any = Math.random() * 10;
+  const n07: any = Math.random() * 10;
+  const n08: any = Math.random() * 10;
+  const n09: any = Math.random() * 10;
+  const n10: any = Math.random() * 10;
+  const n11: any = Math.random() * 10;
+
+  const conteudo = `${parseInt(n01)}${parseInt(n02)}${parseInt(n03)}.${parseInt(
+    n04,
+  )}${parseInt(n05)}${parseInt(n06)}.${parseInt(n07)}${parseInt(n08)}${parseInt(
+    n09,
+  )}-${parseInt(n10)}${parseInt(n11)}`;
 
   return (
     <>
@@ -17,7 +63,20 @@ export default function Gerador() {
         <br />
         <input onClick={gerar} type="button" value="GERAR CPF" />
       </div>
-      {renderiza == true && <Progresso />}
+      {renderiza == true && (
+        <>
+          <div className={style.load}>
+            <progress value="0" max="100"></progress>
+            <span>0%</span>
+          </div>
+        </>
+      )}
+      {renders == true && (
+        <div className={style.cpfContainer}>
+          Aqui está o seu CPF:
+          <div className={style.cpf}>{conteudo}</div>
+        </div>
+      )}
     </>
   );
 }
